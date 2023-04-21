6910d3d1014534eec320272138653ea9b257b69d
sk <jsckhan@outlook.com>
2023-04-21 15:51:26 -0400

    feat: update saad-card

--- src/client/src/components/Saad.tsx
+++ src/client/src/components/Saad.tsx
@@ -1,12 +1,4 @@
 export default function SaadCard() {
-  /**
-    blue bg
-    border
-    rounded border
-    64px padding top
-    173px padding left-right
-    52px margin left
-  */
   return (
     <div className="bg-blue-500 border border-2 rounded-full pt-16 px-[173px] ml-[52px] ">
       This is Saad.
