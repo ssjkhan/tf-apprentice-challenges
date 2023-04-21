3b6f2f77091b841f9220ff6cc23d7288d4e9efa1
Oksana Kalyniak <101350351+Oksanka25@users.noreply.github.com>
2023-04-21 16:10:56 -0400

    oksana-card (#5)

--- src/client/src/app.tsx
+++ src/client/src/app.tsx
@@ -1,11 +1,12 @@
 import SaadCard from "./components/Saad";
-
+import OksanaCard from "./components/Oksana";
 export function App() {
   return (
     <div className="w-screen h-screen flex bg-red-500">
       <div className="mx-auto my-auto pb-16 text-white text-2xl">
         Clean App
         <SaadCard />
+        <OksanaCard />
       </div>
     </div>
   );
