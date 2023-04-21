cbafd62f5e79367de7a7beab6da20a5222d078a9
sk <jsckhan@outlook.com>
2023-04-21 15:50:19 -0400

    feat: saad-card

--- src/client/src/app.tsx
+++ src/client/src/app.tsx
@@ -1,7 +1,12 @@
+import SaadCard from "./components/Saad";
+
 export function App() {
   return (
     <div className="w-screen h-screen flex bg-red-500">
-      <div className="mx-auto my-auto pb-16 text-white text-2xl">Clean App</div>
+      <div className="mx-auto my-auto pb-16 text-white text-2xl">
+        Clean App
+        <SaadCard />
+      </div>
     </div>
   );
 }
