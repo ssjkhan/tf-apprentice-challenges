cd107bc8c20917f476a09156d29f5ff8069a31b5
newspaperhobo <hoboinabox@gmail.com>
2023-04-21 15:57:42 -0400

    feat: kira-card

--- src/client/src/app.tsx
+++ src/client/src/app.tsx
@@ -1,7 +1,11 @@
+import KiraCard from "./components/Kira";
+
 export function App() {
   return (
-    <div className="w-screen h-screen flex bg-red-500">
-      <div className="mx-auto my-auto pb-16 text-white text-2xl">Clean App</div>
+      <div className="w-screen h-screen flex bg-red-500">
+      <KiraCard />
+      <div className="mx-auto my-auto pb-16 text-white text-2xl">Clean App
+      </div>
     </div>
   );
 }
