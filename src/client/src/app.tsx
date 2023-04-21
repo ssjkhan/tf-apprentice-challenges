c83ff99adcd09e753135b4d7aceade16e172291d
aerlikh <aerlikh2011@gmail.com>
2023-04-21 15:50:38 -0400

    anastasiia card

--- src/client/src/app.tsx
+++ src/client/src/app.tsx
@@ -1,7 +1,10 @@
+import AnastasiiaCard from '../components/Anastasiia'
+
 export function App() {
   return (
     <div className="w-screen h-screen flex bg-red-500">
       <div className="mx-auto my-auto pb-16 text-white text-2xl">Clean App</div>
+      <AnastasiiaCard />
     </div>
   );
 }
